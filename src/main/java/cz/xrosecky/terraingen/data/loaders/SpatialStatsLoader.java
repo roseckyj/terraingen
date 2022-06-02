1ec802d6666bafb830425300cdee520aaf617152
roseckyj <rosecky.jonas@centrum.cz>
2022-06-02 16:23:22 +0200

    Removed spatial stats data loader

--- src/main/java/cz/xrosecky/terraingen/data/loaders/SpatialStatsLoader.java
+++ src/main/java/cz/xrosecky/terraingen/data/loaders/SpatialStatsLoader.java
@@ -28,56 +28,6 @@ public class SpatialStatsLoader extends AbstractLoader {
 
     @Override
     public void LoadRegion(Point2D from, Point2D to) {
-        try {
-            Statement s = conn.createStatement();
-            ResultSet r = s.executeQuery(String.format("SELECT geom, pocet_pm FROM brno_employment WHERE geom && %s", DatabaseUtils.CornersToGeom(from, to)));
 
-            while (r.next()) {
-                MultiPolygon geom = (MultiPolygon) ((PGgeometry)r.getObject(1)).getGeometry();
-                int workspaces = r.getInt(2);
-
-                int minX = Integer.MAX_VALUE;
-                int maxX = Integer.MIN_VALUE;
-                int minZ = Integer.MAX_VALUE;
-                int maxZ = Integer.MIN_VALUE;
-
-                for (int i = 0; i < geom.numPolygons(); i++) {
-                    Polygon p = geom.getPolygon(i);
-                    for (int j = 0; j < p.numPoints(); j++) {
-                        Pointf3D pt = Coords.latLonToXZ(new Pointf3D(p.getPoint(j)));
-                        minX = Math.min(minX, (int)Math.floor(pt.x));
-                        maxX = Math.max(maxX, (int)Math.ceil(pt.x));
-                        minZ = Math.min(minZ, (int)Math.floor(pt.z));
-                        maxZ = Math.max(maxZ, (int)Math.ceil(pt.z));
-                    }
-                }
-
-                minX = (int)Math.floor(minX / 16.0);
-                maxX = (int)Math.ceil(maxX / 16.0);
-                minZ = (int)Math.floor(minZ / 16.0);
-                maxZ = (int)Math.ceil(maxZ / 16.0);
-
-                for (int x = minX; x <= maxX; x++) {
-                    for (int z = minZ; z <= maxZ; z++) {
-
-                        Point2D fetchPoint = new Point2D(x, z);
-                        if (new Point2D(x * 16, z * 16).within(from, to)) {
-                            DataChunk chunk = storage.chunks.get(fetchPoint);
-
-                            if (!chunk.spatialStats.containsKey("workspaces")) {
-                                chunk.spatialStats.put("workspaces", new ArrayList<SpatialData>());
-                            }
-
-                            chunk.spatialStats.get("workspaces").add(new SpatialData(geom, new HashMap<String, Integer>(){{
-                                put("workspaces", workspaces);
-                            }}));
-                        }
-                    }
-                }
-            }
-            s.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
