46e67c2362a4a9c41cbd8f9db8abcbbbe3e176bf
roseckyj <rosecky.jonas@centrum.cz>
2022-06-02 16:12:39 +0200

    Removed loop workplaces rendering

--- src/main/java/cz/xrosecky/terraingen/Loop.java
+++ src/main/java/cz/xrosecky/terraingen/Loop.java
@@ -32,23 +32,23 @@ public class Loop implements Runnable {
 
     @Override
     public void run() {
-        for (BossBar b : bars) {
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                p.hideBossBar(b);
-            }
-        }
-        bars = new ArrayList<BossBar>();
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            Location l = p.getLocation();
-            SpatialData d = storage.getChunk((int)Math.floor(l.getX() / 16.0), (int)Math.floor(l.getZ() / 16.0)).getData("workspaces", (int)l.getX(), (int)l.getZ());
-
-            if (d != null) {
-                final Component name = Component.text(d.data.get("workspaces") + " volných pracovních míst v této oblasti");
-                final BossBar bossBar = BossBar.bossBar(name, 1, BossBar.Color.GREEN, BossBar.Overlay.PROGRESS);
-
-                p.showBossBar(bossBar);
-                bars.add(bossBar);
-            }
-        }
+//        for (BossBar b : bars) {
+//            for (Player p : Bukkit.getOnlinePlayers()) {
+//                p.hideBossBar(b);
+//            }
+//        }
+//        bars = new ArrayList<BossBar>();
+//        for (Player p : Bukkit.getOnlinePlayers()) {
+//            Location l = p.getLocation();
+//            SpatialData d = storage.getChunk((int)Math.floor(l.getX() / 16.0), (int)Math.floor(l.getZ() / 16.0)).getData("workspaces", (int)l.getX(), (int)l.getZ());
+//
+//            if (d != null) {
+//                final Component name = Component.text(d.data.get("workspaces") + " volných pracovních míst v této oblasti");
+//                final BossBar bossBar = BossBar.bossBar(name, 1, BossBar.Color.GREEN, BossBar.Overlay.PROGRESS);
+//
+//                p.showBossBar(bossBar);
+//                bars.add(bossBar);
+//            }
+//        }
     }
 }
